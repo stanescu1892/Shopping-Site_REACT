914ed55600d401f88af0798693e7124ac7d75238
Alexandru Stanescu <alexandru.stanescu97@gmail.com>
2022-04-27 21:00:56 +0300

    added header component and links to home

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 // import Link from "link-react";
+import Header from "./components/header/Header";
 import Shop from "./pages/shop/shop";
 
 import Homepage from "./pages/Homepage/Homepage";
 
 function App() {
   return (
+   
     <Router>
+       <Header/>
       <div>
         <Routes>
           <Route index path="/" element={<Homepage />} />
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,25 @@
+import "./Header.scss";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+
+function Header() {
+  return (
+    <div className="header">
+      <Link className="logo-container" to="/">
+        <Logo className="logo" />
+      </Link>
+      <div className="options">
+        <Link className="option" to="/shop">
+        
+          SHOP
+        </Link>
+        <Link className="option" to="/shop">
+        
+         Contact
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
